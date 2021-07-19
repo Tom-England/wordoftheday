56d95c676c9e8e970cb127bfb46d16f6f42383c5
Tom-England <tomengland6679@gmail.com>
2021-07-19 20:53:05 +0100

    Added textbox for word

--- wordoftheday.lua
+++ wordoftheday.lua
@@ -2,3 +2,14 @@
 -- By Tom England
 -- Provided as is
 -- For word list, try words on Arch or wordlist on debian/ubuntu
+
+local wibox = require("wibox")
+
+local wordofthedaywidget = wibox.widget{
+	text='test',
+	align='center',
+	valign='center',
+	widget=wibox.widget.textbox
+}
+
+return wordofthedaywidget
