ea002ad77fb5b2f2546c30f64e578b7562f6aa8f
Tom-England <tomengland6679@gmail.com>
2021-09-24 17:11:28 +0100

    Updated file handling so file actually opens

--- wordoftheday.lua
+++ wordoftheday.lua
@@ -7,19 +7,42 @@ local wibox = require("wibox")
 
 local function open_file(filename)
 	file = io.open(filename)
-	io.input(file)
 	return file
 end
 
-local function get_word()
-	return 'test'
+local function get_lines_in_file(filename)
+	file = open_file(filename)
+	t = file:read("*a")
+	_,n = t:gsub("\n","")
+	return n
 end
 
+local function get_date()
+		return os.date("%d%m%Y")
+end
+
+local function hash_date(date, lines)
+		return date % lines
+end
+
+local function get_word(file, hash)
+	io.input(io.open(file))
+	for i=0, hash, 1
+		do
+			io.read()
+	end
+	line = io.read()
+	io.close()
+	return line
+end
+
+local filename = "/usr/share/dict/words"
+
 local wordofthedaywidget = wibox.widget{
-	text=get_word(),
-	align='center',
-	valign='center',
-	widget=wibox.widget.textbox
-}
+		text=get_word(filename, hash_date(get_date(), get_lines_in_file(filename))),
+		align='center',
+		valign='center',
+		widget=wibox.widget.textbox
+	}
 
 return wordofthedaywidget
